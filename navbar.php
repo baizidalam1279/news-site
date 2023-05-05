6337c118e12702850455eaa22688b2c729d54fca
Muhammad Baizid <Baizid@Jaa_alhaque.com>
2023-05-06 01:25:55 +0600

    first commit

---
+++ navbar.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+    <meta charset="utf-8">
+        <title>news site</title>
+            <style media="screen">
+            body{
+        padding: 20px;
+        margin: 0px;
+        }
+        ul {
+            list-style-type: none;
+            background-color: teal;
+            width: 100%;
+            height: 50px;
+            margin-top: 0px;
+        }
+        li{
+            float: left;
+            margin-left: 30px;
+            margin-top: 10px;
+        }
+        li:hover{
+            opacity: 0.8;
+        }
+        a{
+            text-decoration: none;
+            color: white;
+            font-size: 20px;
+            font-weight: bold;
+        }
+        </style>
+</head>
+<body>
+<ul>
+    <li style="margin-left:0px;"> <a href="#">news sharing site</a> </li>
+    <li style="margin-left:800px;"> <a href="#">home</a> </li>
+    <li> <a href="#">contact</a> </li>
+    <li> <a href="#">About</a> </li>
+    <li> <a href="#">Service</a> </li>
+    <li> <a href="#">Update</a> </li>
+
+</ul>
+
+
+    </body>
+</html>
